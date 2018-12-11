9c11ac115605d6e3bd9dddb854f7a370175beb5d
Hiếu Trương Quốc <quochieu4497@gmail.com>
2018-12-11 09:12:06 +0700

    update

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+var ho = "Nguyen"
+var lot = "Pham"
+var ten = "Bao"
+var result = 'Hello '+ho+' how\'re you '+' ----- '+lot+'\\'+ten+' ------'
+console.log(result)
+var con = "~!@#$%^&*()_+{}|:|\":?></.,"
+console.log(con)
